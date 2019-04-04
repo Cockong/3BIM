274706b500828b9d51e8e150ced7e786564ccdd7
Cockong <45364934+Cockong@users.noreply.github.com>
2019-04-04 16:00:45 +0200

    add lokta voltera

--- test_loktavolterra.R
+++ test_loktavolterra.R
@@ -7,11 +7,11 @@ rm(list=ls()) #reinitialiser variables
 Eq3 = function(t,x,parameters) #modèle complexe
 {
   with(as.list(c(parameters,x)),{
-    dS = - beta*S*I  + alpha*(S+I+R) -lambda*S - theta*(S+I+R)*S/k
-    dI =   beta*S*I - gamma*I - mu*I -lambda*I - theta*(S+I+R)*I/k
-    dR =   mu*I - lambda*R - theta*(S+I+R)*R/k
+    dS = - beta*S*I  + alpha*(S+I+R) -lambda*S - theta*(S+I+R)*S/k + proies_necessaires*probabilite_rencontre*S*P
+    dI =   beta*S*I - gamma*I - mu*I -lambda*I - theta*(S+I+R)*I/k + proies_necessaires*probabilite_rencontre*I*P
+    dR =   mu*I - lambda*R - theta*(S+I+R)*R/k + proies_necessaires*probabilite_rencontre*R*P
     dD =   gamma*I + lambda*(R+I+S) + theta*(S+I+R)^2/k
-    dP =   sigma*P*(1-P/kp )  - omega*(S+I+R)*P
+    dP =   sigma*P*(1-P/kp )  - probabilite_rencontre*(S+I+R)*P #on estime quune rencontre = 1 mort
     list(c(dS,dI,dR,dD,dP))
   })
 }
@@ -21,7 +21,7 @@ Eq3 = function(t,x,parameters) #modèle complexe
 
 
 simulation_logistique = function(ini,inisansmaladie,parameters,maxtime,pasdetemps,
-                                 ploting = TRUE, ylim=c(0,max(parameters['N'],parameters['k'])))
+                                 ploting = TRUE, ylim=10*c(0,max(parameters['N'],parameters['k'])))
 {
   #simulation sans infectés initiaux
   
@@ -61,7 +61,9 @@ simulation_logistique = function(ini,inisansmaladie,parameters,maxtime,pasdetemp
     I=0                               #round(as.numeric(sol[fin_epidemie,"S"]))
     R=as.numeric(sol[fin_epidemie,"R"])
     D=as.numeric(sol[fin_epidemie,"D"])
-    continuer=c(S=S,I=I,R=R,D=D)
+    P=as.numeric(sol[fin_epidemie,"P"])
+    
+    continuer=c(S=S,I=I,R=R,D=D,P=P)
     time2=seq(0,maxtime-time[fin_epidemie],pasdetemps) # temps moins important
     
     sol2=lsoda(func=Eq3,y=continuer,times=time2,parms = parameters) #resoud
@@ -73,6 +75,8 @@ simulation_logistique = function(ini,inisansmaladie,parameters,maxtime,pasdetemp
     imaladie=c(sol[1:fin_epidemie-1,"I"],sol2[,"I"])
     rmaladie=c(sol[1:fin_epidemie-1,"R"],sol2[,"R"])
     dmaladie=c(sol[1:fin_epidemie-1,"D"],sol2[,"D"])
+    pmaladie=c(sol[1:fin_epidemie-1,"P"],sol2[,"P"])
+    
   }
   else
   {
@@ -81,6 +85,7 @@ simulation_logistique = function(ini,inisansmaladie,parameters,maxtime,pasdetemp
     imaladie=sol[,"I"]
     rmaladie=sol[,"R"]
     dmaladie=sol[,"D"]
+    pmaladie=sol[,"P"]
     morts_avec_maladie=sol[length(sol[,"D"]),"D"]
     morts_pendant_maladie=morts_avec_maladie
   }
@@ -104,7 +109,13 @@ simulation_logistique = function(ini,inisansmaladie,parameters,maxtime,pasdetemp
     points(x = time,smaladie+imaladie+rmaladie,col="purple",type="l")
     legend("topright", legend=c("S","I","R","D","k","Nmaladie"), col=c("green","red","blue","black","brown","purple"),lty=1)
     abline(h=parameters['k'],col="brown")
+
     
+        
+    plot(x=time,y=pmaladie,xlab="Temps",ylab="Population",type="l",ylim=c(0,3000),col="green",main = "Comparaison Population de proies")
+    points(x = time,solm[,"P"],col="red",type="l")
+    legend("topright", legend=c("Pmaladie","Psansmaladie"), col=c("green","red"),lty=1)
+    abline(h=parameters['kp'],col="brown")
   }
   
   
@@ -137,9 +148,8 @@ simulation_logistique = function(ini,inisansmaladie,parameters,maxtime,pasdetemp
 
 
 #Données espérance positive
-
 beta=0.00225 #infectiosité
-gamma=0.0004 #mortalité
+gamma=0.04 #mortalité
 mu=0.095 #recouvrement
 N=300 #pop initiale
 I=1 #infecté initiale
@@ -147,10 +157,20 @@ lambda=0.001 #morts naturelles
 alpha=0.002 #naissances naturelles
 theta=alpha-lambda #pression démographique
 k=800 #capacité limite
-parameters=c(beta=beta,gamma=gamma,N=N,mu=mu,lambda=lambda,alpha=alpha,theta=theta,k=k)
-ini=c(S=N-I,I=I,R=0,D=0)
-inisansmaladie=c(S=N,I=0,R=0,D=0)
-maxtime=4000
+
+#paramètre des proies
+proies_necessaires =4
+probabilite_rencontre=10^-6
+M=N*4 #population initiale proie
+kp=N*12 #capacité limite proie
+sigma= 0.002 #taux d'accroissement naturelle des proies
+
+
+parameters=c(beta=beta,gamma=gamma,N=N,mu=mu,lambda=lambda,alpha=alpha,theta=theta,k=k,
+             proies_necessaires =proies_necessaires,probabilite_rencontre=probabilite_rencontre,kp=kp,sigma=sigma)
+ini=c(S=N-I,I=I,R=0,D=0,P=M)
+inisansmaladie=c(S=N,I=0,R=0,D=0,P=M)
+maxtime=2000
 pasdetemps=0.1
 
 
@@ -160,6 +180,7 @@ pasdetemps=0.1
 
 #en initialisant sansinfecté dans les deux cas on obtient bien les mème résultats
 #De plus on observe bien els résultats attends pour une population suivant le modèle logistique
+par(mfrow=c(2,1))
 
 testblanc=simulation_logistique(ini=inisansmaladie ,inisansmaladie=inisansmaladie,
                                 parameters=parameters,maxtime=maxtime,pasdetemps=pasdetemps)
@@ -184,7 +205,7 @@ print(testblanc)
 # generalement espérance de vie avec maladie < espérance de vie sans maladie 
 #V ATTENTION CE RESULTAT VARIE EN FONCTION DES TESTS notemment quand mortalité maladie<< taux de recouvrement et que maladie non endémique
 #D'après résultat taux de natalité trop élevé 
-par(mfrow=c(2,1))
+par(mfrow=c(2,2))
 
 parameters=c(beta=beta,gamma=gamma,N=N,mu=mu,lambda=lambda,alpha=alpha,theta=theta,k=N*2) #capacité limite > Pop départ
 testksup=simulation_logistique(ini=ini,inisansmaladie=inisansmaladie,parameters=parameters,
