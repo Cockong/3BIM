d20123c9d10f9868d9ccf3d7a302c1ce9916278d
jonathanlsn <jonathan.lsn32@gmail.com>
2019-04-25 19:06:00 +0200

    Cette version utilise uniquement des matrices de leslie pour modéliser l'évolution de la population durant l'épidémie. Les paramètres ont été choisis au hasard pour tester le programme.

--- leslie.py
+++ leslie.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Mar 28 20:55:33 2019
+Created on Wed Apr 24 15:59:04 2019
 
-@author: jlouison
+@author: tijod
 """
-import classe as cl
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -17,59 +17,63 @@ N3=120
 N4=56
 
 
-# initialisation de l'épidémie 
-enfant=cl.enfants(N1,N1,0,0)
-ado=cl.ados(N2,N2,0,0)
-adulte=cl.adultes(N3,N2,0,0)
-vieu=cl.vieux(N4,N2,0,0)
+#Initialisation de la matrice de population
+pop=np.array([[N1,N2,N3,N4],[0,0,0,0],[0,0,0,0]])
 
-# Fonction 
+
+# Paramètres 
+beta=0.00225 #infectiosité
+gamma=0.3 #mortalité
+mu=0.095 #recouvrement
+lambda0=0.02 #morts naturelles
+alpha=0.1 #naissances naturelles
+theta=alpha-lambda0 #pression démographique
+k=800 #capacité limite
+c1,c2,c3=0.2,0.09,0.1
+maxtime=100   
+pasdetemps=0.1
+
+# Fonctions
 #Prend en entrée les population de chaque classe d'age et retourne la population restante après un pas de temps de l'épidémie
-def epidemie(N1,N2,N3,N4):
-    enfant.disease()
-    ado.disease()
-    adulte.disease()
-    vieu.disease()
-    N1,N2,N3,N4=enfant.N1,ado.N2,adulte.N3,vieu.N4       # Si arrondissement, utiliser round pour N1,N2,N3 et N4
-    return(N1,N2,N3,N4)
+def epidemie(matrix):
+    S_=sum(matrix[0,:])
+    I_=sum(matrix[1,:])
+    R_=sum(matrix[2,:])
+    leslie=np.array([[-beta*I_+alpha-lambda0-theta*S_/k,alpha-theta*S_/k,alpha-theta],[beta*I_-theta*I_/k,-gamma-mu-lambda0-theta*I_/k,-theta*I_/k],[-theta*R_/k,mu-theta*R_/k,lambda0-theta*R_/k]]) 
+    matrix=matrix+np.dot(leslie,matrix)
+    return(matrix)
 
-#facteurs de croissance des populations
-f1,f2,f3=0.7,0.2,0.5
-d1,d2,d3=0.1,0.3,0.2
-
-
-leslie=np.array([[0,f1,f2,f3],[d1,0,0,0],[0,d2,0,0],[0,0,0,d3]])
-pop=np.array(list(epidemie(N1,N2,N3,N4)))
+# Cette fonction prend en compte la le passage des individus d'une classe d'âge à une autre.
+def demographie(matrix):
+    leslie2=np.array([[-c1,0,0,0],[c1,-c2,0,0],[0,c2,-c3,0],[0,0,c3,0]])
+    matrix=np.transpose(matrix)+np.dot(leslie2,np.transpose(matrix))
+    return(np.transpose(matrix))
 
 
 #initialisation des listes
-POP=[list(pop)]
-POP_tot=[sum(list(pop))]
+POP=[[sum(pop[:,0]),sum(pop[:,1]),sum(pop[:,2]),sum(pop[:,3])]]
+POP_tot=[sum(POP[0])]
 
-
-n=100   #Nombre de pas
 
 # Utilisation de la matrice de Leslie, croissance de population
-for i in range(n):
-    pop=pop+leslie.dot(pop)
-    pop=list(epidemie(pop[0],pop[1],pop[2],pop[3]))
-    POP.append(pop)
-    POP_tot.append(sum(pop))
+for i in range(maxtime):
+    pop=epidemie(pop)
+    pop=demographie(pop)
+    POP.append([sum(pop[:,0]),sum(pop[:,1]),sum(pop[:,2]),sum(pop[:,3])])
+    POP_tot.append(sum(POP[i+1]))
 
 
 #graphique
 
-time=[i for i in range(n+1)]
+time=[i for i in range(maxtime+1)]
 
 plt.xlabel('temps')
 plt.ylabel('population')
 
-plt.plot(time,list(np.array(POP)[:,0]))
-plt.plot(time,list(np.array(POP)[:,1]))
-plt.plot(time,list(np.array(POP)[:,2]))
-plt.plot(time,list(np.array(POP)[:,3]))
+plt.plot(time,np.array(POP)[:,0],label="enfant")
+plt.plot(time,np.array(POP)[:,1],label="ados")
+plt.plot(time,np.array(POP)[:,2],label="adultes")
+plt.plot(time,np.array(POP)[:,3],label="vieux")
 
-plt.plot(time,POP_tot,'r--')
-
-
-
+plt.plot(time,POP_tot,'r--',label="population totale")
+plt.legend()
