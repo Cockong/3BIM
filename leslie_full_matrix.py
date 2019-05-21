1f3a87d66380850e67ed7968a5241f0c1a556aee
jonathanlsn <jonathan.lsn32@gmail.com>
2019-05-21 15:58:31 +0200

    modifs sur le fichier full_matrix

--- leslie_full_matrix.py
+++ leslie_full_matrix.py
@@ -10,10 +10,10 @@ import matplotlib.pyplot as plt
 
 
 #Population initiale pour les 4 classes d'individus
-N1=50    # Nombre d'enfants initial
-N2=70    # Nombre d'ados initial
-N3=120   # Nombre d'adultes initial
-N4=56    # Nombre de vieux initial
+N1=500    # Nombre d'enfants initial
+N2=540   # Nombre d'ados initial
+N3=458   # Nombre d'adultes initial
+N4=354    # Nombre de vieux initial
 
 
 #Initialisation de la matrice de population
@@ -35,7 +35,7 @@ alpha=0.1   #taux de naissances naturelles
 theta=alpha-lambda0   #pression démographique
 k=800   #capacité limite
 croissance1,croissance2,croissance3=0.2,0.09,0.1    # taux de croissance des enfants, ados et adultes
-maxtime=10    #Durée en jours, mois ou années?
+maxtime=100    #Durée en jours, mois ou années?
 pasdetemps=0.1   
 
 # Matrice de Leslie statiques
@@ -43,18 +43,17 @@ leslie=np.array([[-croissance1,croissance1,0,0],            # Les coefficients c
                  [0,-croissance2,croissance2,0],
                  [0,0,-croissance3,croissance3],
                  [0,0,0,0]])
+
 Mnatalité=np.array([[0,0,0,0],
                    [0,0,0,0],
                    [alpha,0,0,0],
                    [alpha,0,0,0]])
-Msansmort=np.array([[1,0,0,0],
-                   [0,1,0,0],
-                   [0,0,1,0],
-                   [0,0,0,0]])
+
 Mbébésain=np.array([[1,1,1,0],
                    [0,0,0,0],
                    [0,0,0,0],
                    [0,0,0,0]])
+
 sup_death=np.array([[1,0,0,0],
                     [0,1,0,0],
                     [0,0,1,0],
@@ -62,54 +61,76 @@ sup_death=np.array([[1,0,0,0],
 
 # Fonctions
 #Prend en entrée les population de chaque classe d'age et retourne la population restante après un pas de temps de l'épidémie
-def epidemie(matrix):
+
+
+
+def demographie(matrix,POP_tot):
+    N=POP_tot[-1]
     S_=sum(matrix[0,:])     # nombre total de personnes saines dans la population
     Mépidémie=np.array([[0,-beta*S_,0,0],
                         [0,beta*S_-gamma-mu,0,0],
                         [0,mu,0,0],
                         [0,gamma,0,0]]) 
-    matrix=matrix+np.dot(Mépidémie,matrix)        # M=M+L*M : met à jours le nombre de sains, d'infectés, d'immunisés et de morts à chaque pas de temps
-    return(matrix)
-
-# Cette fonction prend en compte la le passage des individus d'une classe d'âge à une autre.
-def demographie(matrix):
-    N=POP_tot[-1]
+    
     Mmortalité=np.array([[-lambda0-theta*N/k,0,0,0],
                        [0,-lambda0-theta*N/k,0,0],
                        [0,0,-lambda0-theta*N/k,0],
                        [lambda0+theta*N/k,lambda0+theta*N/k,lambda0+theta*N/k,0]])
-    matrix=matrix+np.dot(np.dot(sup_death,matrix),leslie)+np.dot(np.dot(Mbébésain,matrix),Mnatalité)+np.dot(Mmortalité,matrix)       
+    matrix=matrix+np.dot(Mépidémie,matrix) +np.dot(np.dot(sup_death,matrix),leslie)+np.dot(np.dot(Mbébésain,matrix),Mnatalité)+np.dot(Mmortalité,matrix)       
     return(matrix)
 
 
-#initialisation des listes
-POP=[[sum(pop[:3,0]),sum(pop[:3,1]),sum(pop[:3,2]),sum(pop[:3,3])]]
-POP_tot=[sum(POP[0])]
-Death=[sum(pop[3,:])]
-#Death=[sum()]
-
-
-# Utilisation de la matrice de Leslie, croissance de population
-for i in range(maxtime):
-    pop=epidemie(pop)
-    pop=demographie(pop)
-    POP.append([sum(pop[:3,0]),sum(pop[:3,1]),sum(pop[:3,2]),sum(pop[:3,3])])
-    POP_tot.append(sum(POP[i+1]))
-    Death.append(sum(pop[3,:]))
-
-
-#graphique
-
-time=[i for i in range(maxtime+1)]
-
-plt.xlabel('temps')
-plt.ylabel('population')
-
-plt.plot(time,np.array(POP)[:,0],label="enfant")
-plt.plot(time,np.array(POP)[:,1],label="ados")
-plt.plot(time,np.array(POP)[:,2],label="adultes")
-plt.plot(time,np.array(POP)[:,3],label="vieux")
-plt.plot(time,np.array(Death),'b--',label="morts")
-
-plt.plot(time,POP_tot,'r--',label="population totale")
-plt.legend()
+def simulation(pop):
+    #initialisation des listes
+    evolution_population_avec_maladie=[pop]
+    POP=[[sum(pop[:3,0]),sum(pop[:3,1]),sum(pop[:3,2]),sum(pop[:3,3])]]
+    POP_tot=[sum(POP[0])]
+    Death=[sum(pop[3,:])]
+    #Death=[sum()]
+    
+    
+    # Utilisation de la matrice de Leslie, croissance de population
+    for i in range(maxtime):
+        pop=demographie(pop,POP_tot)
+        POP.append([sum(pop[:3,0]),sum(pop[:3,1]),sum(pop[:3,2]),sum(pop[:3,3])])
+        POP_tot.append(sum(POP[i+1]))
+        Death.append(sum(pop[3,:]))
+        evolution_population_avec_maladie.append(pop)
+    
+    
+    #graphique classe d'âge
+    
+    time=[i for i in range(maxtime+1)]
+    
+    plt.xlabel('temps')
+    plt.ylabel('population')
+    
+    plt.plot(time,np.array(POP)[:,0],label="enfant")
+    plt.plot(time,np.array(POP)[:,1],label="ados")
+    plt.plot(time,np.array(POP)[:,2],label="adultes")
+    plt.plot(time,np.array(POP)[:,3],label="vieux")
+    plt.plot(time,np.array(Death),'b--',label="morts")
+    plt.axhline(y=k, color='k' )
+    
+    plt.plot(time,POP_tot,'r--',label="population totale")
+    plt.legend()
+    plt.close()
+    # graphique SIR
+    
+    plt.xlabel('temps')
+    plt.ylabel('population')
+    
+    print(np.shape(evolution_population_avec_maladie[:][0][1]))
+    print(np.shape(time))
+    plt.plot(time,evolution_population_avec_maladie[:][0][1],label="enfant infecte")
+    plt.plot(time,np.array(POP)[:,1],label="ados")
+    plt.plot(time,np.array(POP)[:,2],label="adultes")
+    plt.plot(time,np.array(POP)[:,3],label="vieux")
+    plt.plot(time,np.array(Death),'b--',label="morts")
+    plt.axhline(y=k, color='k' )
+    
+    plt.plot(time,POP_tot,'r--',label="population totale")
+    plt.legend()
+    plt.close()
+    
+simulation(pop)
